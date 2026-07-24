21dc0a26dc663b031fd1982f99c8fff9d1247e6f
Aryan <aryan1118.be23@chitkarauniversity.edu.in>
2026-07-24 15:56:15 +0530

    UI Polish: Add smooth page transition to Home component

--- image-main/frontend/src/pages/Home.jsx
+++ image-main/frontend/src/pages/Home.jsx
@@ -3,17 +3,22 @@ import GenerateBtn from "../components/GenerateBtn"
 import Header from "../components/Header"
 import Steps from "../components/Steps"
 import Testimonials from "../components/Testimonials"
+import { motion } from 'framer-motion'
 
 
 function Home() {
   return (
-    <div>
+    <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+    >
      <Header/>
      <Steps/>
      <Description/>
      <Testimonials/>
      <GenerateBtn/>
-    </div>
+    </motion.div>
   )
 }
 
